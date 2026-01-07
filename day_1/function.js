6e86cdf1cfbd43ce63e041e0409021c229443c57
Saurav <ghimiresaurav789@gmail.com>
2026-01-07 13:16:28 +0545

    feat : learn regular and arrow function

---
+++ day_1/function.js
@@ -0,0 +1,18 @@
+// function calcuateArea (length, width) {
+//     const cal = length * width;
+
+//     return cal;
+// }
+
+// const newcal = calcuateArea(10, 50);
+// console.log(newcal);
+
+
+const dollars = 20;
+
+const toRupees = ((dollars) => {
+    return dollars * 130;
+})
+
+const d = toRupees(dollars)
+console.log(d);
