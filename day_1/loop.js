8891c043303879ec666d7884bd2e678b1154fbe9
Saurav <ghimiresaurav789@gmail.com>
2026-01-07 16:40:00 +0545

    chore: learning about while and switch

--- day_1/loop.js
+++ day_1/loop.js
@@ -1,6 +1,35 @@
-for (let i = 5; i>0; i--) {
-    console.log('coundowub' + i);
+// for (let i = 5; i>0; i--) {
+//     console.log('coundowub' + i);
+// };
+
+// console.log('lift');
+
+// for (let i = 9; i > 0; i --){
+//     if (i === 3) {
+//         break;
+//     }
+//     console.log("print: " + i);
+// };
+
+// let progress = 0;
+
+// while (progress < 100) {
+//     console.log("dowlanding: " + progress);
+//     progress += 20;
+// };
+
+let pass = "";
+const correctPassword = "1234";
+let attempts = 0;
+
+while (pass !== correctPassword) {
+    attempts++;
+    console.log("attempt" + attempts+ "guess");
+
+    if (attempts === 3){
+        pass = "1234";
+    }
+    
 }
 
-console.log('lift');
-
+console.log("accses: "+ attempts);
