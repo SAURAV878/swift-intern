6ef90bdee073b4b97a98546f5889f42b0da2856b
Saurav <ghimiresaurav789@gmail.com>
2026-01-14 14:24:29 +0545

    feat: learn retrun vs console.log diifernce

---
+++ environment/function.js
@@ -0,0 +1,25 @@
+// const greet = (name) => {
+//     console.log("Hello " + name);
+// };
+
+// greet("Saurav"); 
+// greet();         
+
+// const calculateBill = (price, tax= 0.10) => {
+//     return price + (price * tax);
+// }
+
+// console.log(calculateBill(100));
+// console.log(calculateBill(100, 0.20));
+
+// const multiplyByTwo = (num) => {
+    
+//     return num * 2;
+// };
+
+// const addFive = (num) => {
+    
+//     return num + 5;
+// };
+// const final = addFive(multiplyByTwo(10));
+// console.log('the final result is: ' + final);
--- environment/jsruntime.js
+++ environment/jsruntime.js
@@ -1,82 +1,92 @@
-console.log(a);
-let a = 10 ;
+// console.log(a);
+// let a = 10 ;
 
 
 
-const hello= () => {
-  console.log("Hello");
-}
+// const hello= () => {
+//   console.log("Hello");
+// }
 
-hello();
+// hello();
 
-let x = 10;
-if (true) {
-    let x = 20;
-    console.log("inside: ", x);
-}
+// let x = 10;
+// if (true) {
+//     let x = 20;
+//     console.log("inside: ", x);
+// }
 
-console.log('outside: ', x);
+// console.log('outside: ', x);
 
-if (true) {
-    var y = 50;
-}
+// if (true) {
+//     var y = 50;
+// }
 
-console.log('value of y: ', y);
+// console.log('value of y: ', y);
 
-console.log(myVar);
-var myVar = 'i am here';
-
-
-
-function innerTask () {
-    console.log('2. i am doing the inner task ');
-}
-
-function  outerTask () {
-    console.log('1. Starting the outer task');
-
-    innerTask();
-    console.log('3. finishing the outer task');
-}
-
-console.log('O. start the program');
-outerTask();
-console.log('4. end the program');
-
-secretMessage()
-
-console.log(myBankBalance);
-
-function secretMessage (){
-    console.log('the safe is open');
-}
-
-var myBankBalance = 100;
-
-console.log(mySecretPin);
-let mySecretPin = 1234;
+// console.log(myVar);
+// var myVar = 'i am here';
 
 
-function jailHouse () {
-    if (true){
-        var prisoner = 'i am here locked in '
-    }
-    console.log('inside: prisoner')
 
-}
+// function innerTask () {
+//     console.log('2. i am doing the inner task ');
+// }
 
-jailHouse();
-console.log(prisoner);
+// function  outerTask () {
+//     console.log('1. Starting the outer task');
 
-greeetRegular();
-greetArrow();
+//     innerTask();
+//     console.log('3. finishing the outer task');
+// }
 
-function greeetRegular(){
-    console.log('hey ')
+// console.log('O. start the program');
+// outerTask();
+// console.log('4. end the program');
 
-}
+// secretMessage()
 
-var greetArrow = () => {
-    console.log('you')
+// console.log(myBankBalance);
 
-}
+// function secretMessage (){
+//     console.log('the safe is open');
+// }
+
+// var myBankBalance = 100;
+
+// console.log(mySecretPin);
+// let mySecretPin = 1234;
+
+
+// function jailHouse () {
+//     if (true){
+//         var prisoner = 'i am here locked in '
+//     }
+//     console.log('inside: prisoner')
+
+// }
+
+// jailHouse();
+// console.log(prisoner);
+
+// greeetRegular();
+// greetArrow();
+
+// function greeetRegular(){
+//     console.log('hey ')
+
+// }
+
+// var greetArrow = () => {
+//     console.log('you')
+
+// }
+
+// console.log('1. i am first (stack)');
+
+// setTimeout (() => {
+//     console.log('2. i am second (Queue)');
+
+// }, 0);
+
+// console.log('3. i am third (stack)');
+
