c1a481ec6a2755235c8133f08e28b485825fa0d9
Saurav <ghimiresaurav789@gmail.com>
2026-01-15 22:00:30 +0545

    feat: learn array methods

--- environment/array.js
+++ environment/array.js
@@ -1,19 +1,32 @@
-const queue = [
-    {name: 'ram'},
-    {name: 'sita'},
+// const queue = [
+//     {name: 'ram'},
+//     {name: 'sita'},
+// ];
+
+// queue.push({name: 'Gopal'});
+// queue.unshift({name: 'Saurav'});
+// queue.pop();
+
+// const backQueue = [...queue];
+// backQueue[0].name = 'hacker';
+
+// queue.forEach(user => {
+//     console.log(user);
+// });
+
+const products = [
+    { name: "Phone", price: 500, inStock: true },
+    { name: "Screen", price: 80, inStock: false },
+    { name: "Cable", price: 10, inStock: true },
+    { name: "Laptop", price: 1200, inStock: true }
 ];
 
-queue.push({name: 'Gopal'});
-queue.unshift({name: 'Saurav'});
-queue.pop();
+const availableItems = (products.filter(p => p.inStock  == true));
 
-const backQueue = [...queue];
-backQueue[0].name = 'hacker';
+const taxedPrices = availableItems.map(p => (p.price * 1.10));
+console.log(taxedPrices);
 
-queue.forEach(user => {
-    console.log(user);
-})
-
-
-
-
+const totalBill = taxedPrices.reduce ((total, price) => {
+    return total + price; 
+},0);
+console.log(totalBill);
