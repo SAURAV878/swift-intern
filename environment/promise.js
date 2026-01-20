9068d4e3fd5a350be4bdc7c94d51838408f274bc
Saurav <ghimiresaurav789@gmail.com>
2026-01-20 10:55:35 +0545

    feat: learn async/await modern way of promise

--- environment/promise.js
+++ environment/promise.js
@@ -2,7 +2,7 @@ const checkEntry = ((age) =>{
     return new Promise((resolve, reject) => {
         setTimeout (() => {
             if (age >= 18) {
-                resolve({status : 200, msg: 'welcome', user:'saurav'});
+                resolve('welcome to the club');
             } else {
                 reject('too young! Go home.');
             }
@@ -12,13 +12,31 @@ const checkEntry = ((age) =>{
     });
 });
 
-checkEntry(20)
-    .then((message) => {
-        console.log(message.user);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+// checkEntry(20)
+//     .then((message) => {
+//         console.log(message.user);
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     });
 
+const handleEntry = async () => {
+    try {
+       const message = await checkEntry(29);
+       console.log(message);
+
+    } catch (err) {
+        console.log('it error: ' + err);
+    }
+}
+
+handleEntry();
+
+async function test () {
+    return 'hello';
+    
+}
+
+console.log(test());
     
 
