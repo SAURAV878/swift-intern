9d1ce159c59e62cddee6a3a1466bd10b45aa0dd4
Saurav <ghimiresaurav789@gmail.com>
2026-01-22 21:17:55 +0545

    chore: try to partice another question using .map in immutability.js

--- environment/immutability.js
+++ environment/immutability.js
@@ -1,14 +1,32 @@
 // immutability: means once data is cerated, it never changes ,if you want to change then you have to make a whole new copy 
 
-const originalProfile = {
-    username: 'saurav_ghimire',
-    status: 'active',
-    skills:['JS', 'Node']
-};
+// const originalProfile = {
+//     username: 'saurav_ghimire',
+//     status: 'active',
+//     skills:['JS', 'Node']
+// };
 
-const updatedProfile = {...originalProfile, status: 'learning'};
+// const updatedProfile = {...originalProfile, status: 'learning'};
 
-const finalProfile = {...updatedProfile, skills: [...skills, 'Express']};
+// const finalProfile = {...updatedProfile, skills: [...updatedProfile.skills, 'Express']};
 
-console.log(originalProfile.status);
-console.log(finalProfile.skills);
+// console.log(originalProfile.status);
+// console.log(finalProfile.status);
+// console.log(finalProfile.skills);
+
+
+const cart = [
+    { id: 1, name: "Apple", price: 10 },
+    { id: 2, name: "Banana", price: 5 }
+];
+
+const newCart = cart.map(item => {
+    if (item.id === 1) {
+        return {...item, price: 12};
+    } else {
+        return item
+    }
+});
+
+console.log(cart[0].price);
+console.log(newCart[0].name);
